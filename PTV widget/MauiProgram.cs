a41a275ab7cec4a57ec57817b08defb3282efc15
Simon W <simongwunderlich@gmail.com>
2025-08-30 18:16:58 +1000

    UI design

--- PTV widget/MauiProgram.cs
+++ PTV widget/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace PTV_widget
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("fa.otf", "fa");
                 });
 
 #if DEBUG
