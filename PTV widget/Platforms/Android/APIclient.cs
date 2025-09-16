bc63fb123a83096d69d6f23b7f5e4314fde4bc42
simon <s4172040@student.rmit.edu.au>
2025-09-16 11:12:42 +1000

    Clean up

--- PTV widget/Platforms/Android/APIclient.cs
+++ PTV widget/Platforms/Android/APIclient.cs
@@ -7,6 +7,8 @@ namespace PTV_widget.Platforms.Android
 {
 	public class APIclient
 	{
+		// Calculates signature based on DEV_ID and DEV_KEY
+		// Formats url with signature
 		private static string addCredentials(string url)
 		{
 			// add developer id
@@ -25,55 +27,71 @@ namespace PTV_widget.Platforms.Android
 			return url;
 		}
 
+		// Gets dictionary associating route number and route name
+		// Saves extra api call per route
 		internal static Dictionary<int, string> parseRoutes(JToken json)
 		{
 			Dictionary<int, string> routes = new Dictionary<int, string>();
 			foreach (var route in json["routes"])
 			{
 				int route_num = int.Parse(route["route_id"].ToString());
+				// Skip if not distinct
+				if (routes.ContainsKey(route_num))
+					continue
 				string route_name;
+				// If train, use route name
 				if (route["route_type"].ToString() == "0")
 					route_name = route["route_name"].ToString();
+				// Shorten VLine route names
 				else if (route["route_type"].ToString() == "3")
 				{
 					route_name = route["route_name"].ToString();
 					route_name = Regex.Replace(route_name, " via(.+?)", "fast");
 				}
+				// If bus, nightbus, or tram, use route_number
 				else
 					route_name = route["route_number"].ToString();
-
-				if (!routes.ContainsKey(route_num))
-					routes.Add(route_num, route_name);
+				
+				routes.Add(route_num, route_name);
 			}
 			return routes;
 		}
-
+		// Get stop of a given route type closest to _long and _lat
 		internal async static Task<Stop?> getClosestStop(double _long, double _lat, RouteType route_type)
 		{
+			// Call PTV API endpoint /v3/stops/location/{latitude},{longitude}
 			using var client = new HttpClient(new Xamarin.Android.Net.AndroidMessageHandler());
 			client.BaseAddress = new Uri("https://timetableapi.ptv.vic.gov.au" + addCredentials($"/v3/stops/location/{_lat},{_long}?max_results=1&route_types={(int)route_type}&max_distance=1000"));
 			var response = await client.GetAsync(client.BaseAddress);
+
+			// Parse JSON
 			string jsonString = await response.Content.ReadAsStringAsync();
 			JObject respObj = JObject.Parse(jsonString);
 
+			// Existence check
 			if (respObj["stops"].Count() == 0)
 				return null;
 
+			// Parse id and name
 			int id = int.Parse(respObj["stops"].First()["stop_id"].ToString());
-			int type = int.Parse(respObj["stops"].First()["route_type"].ToString());
-			
 			string name = respObj["stops"].First()["stop_name"].ToString();
 
-			if ((RouteType)type == RouteType.Tram)
+			// If tram stop, store only stop number as stop name
+			if (route_type == RouteType.Tram)
 				name = "Stop #" + Regex.Replace(name, "(.+?)#", "");
+			// If bus-ish stop, only keep unique part of name
+			// Original: {side street} / {main street}
+			// Formatted: {side street}
 			else if ((RouteType)type == RouteType.Bus || (RouteType)type == RouteType.NightBus)
 				name = Regex.Replace(name, "/(.+?)$", "");
 
+			// Gets route name for each route number
 			Dictionary<int, string> routes = parseRoutes(respObj["stops"].First());
 
-			return new Stop(name, id, (RouteType)type, routes);
+			return new Stop(name, id, route_type, routes);
 		}
 
+		// Get list of next distinct departures at a given stop
 		internal async static Task<List<Departure>> getNextDeparture(Stop stop)
 		{
 			try
@@ -81,10 +99,13 @@ namespace PTV_widget.Platforms.Android
 				List<Departure> departures = new List<Departure>();
 				List<string> platforms = new List<string>();
 
+				// Call PTV API endpoint /v3/departures/route_type/{route_type}/stop/{stop_id}
+				// Expand argument gives more detailed object, needed to find direction names
 				using var client = new HttpClient(new Xamarin.Android.Net.AndroidMessageHandler());
 				client.BaseAddress = new Uri("https://timetableapi.ptv.vic.gov.au" + addCredentials($"/v3/departures/route_type/{(int)stop.route_type}/stop/{stop.stop_id}?max_results=1&expand=0"));
-
 				var response = await client.GetAsync(client.BaseAddress);
+
+				//Parse JSON
 				string jsonString = await response.Content.ReadAsStringAsync();
 				JObject respObj = JObject.Parse(jsonString);
 
@@ -93,14 +114,22 @@ namespace PTV_widget.Platforms.Android
 				DateTime eta;
 				bool isAtPlatform;
 				string dest;
+
+				// Parse each departure object
 				foreach (var dep in respObj["departures"])
 				{
+					// Check distinct direction
 					if (platforms.Contains(dep["direction_id"].ToString()))
 						continue;
 
+
 					route_num = int.Parse(dep["route_id"].ToString());
+
+					// Check distinct route
 					if (!stop.routes.ContainsKey(route_num))
 						continue;
+
+					// Parse name, eta datetime UTC format, is at_platform
 					route_name = stop.routes[route_num];
 					string etaStr = dep["estimated_departure_utc"].ToString();
 					if (etaStr == "")
@@ -108,6 +137,7 @@ namespace PTV_widget.Platforms.Android
 					eta = DateTime.Parse(etaStr).ToLocalTime();
 					isAtPlatform = bool.Parse(dep["at_platform"].ToString());
 
+					// Parse direction num and use to find destination name 
 					string dirNum = dep["direction_id"].ToString();
 					var destObj = respObj["directions"][dirNum];
 					dest = destObj["direction_name"].ToString();
--- PTV widget/Platforms/Android/Widget.cs
+++ PTV widget/Platforms/Android/Widget.cs
@@ -11,7 +11,7 @@ namespace Maui.Widgets;
 
 public abstract class Widget : AppWidgetProvider
 {
-
+	// Define info to display on widget for each route type
 	static Dictionary<RouteType, WidgetInfo> info = new Dictionary<RouteType, WidgetInfo>()
 	{
 		{ RouteType.Train, new WidgetInfo()},
@@ -26,11 +26,15 @@ public abstract class Widget : AppWidgetProvider
     {
         var views = new RemoteViews(context.PackageName, Microsoft.Maui.Resource.Layout.widget);
 
+		// Define button press actions
 		views.SetOnClickPendingIntent(PTV_widget.Resource.Id.update, GetPendingSelfIntent(context, "action.UPDATE"));
 		views.SetOnClickPendingIntent(PTV_widget.Resource.Id.nextPlatform, GetPendingSelfIntent(context, "action.NEXT_PLATFORM"));
 		views.SetOnClickPendingIntent(PTV_widget.Resource.Id.prevPlatform, GetPendingSelfIntent(context, "action.PREV_PLATFORM"));
-
+		
+		// Show loading circle
 		views.SetViewVisibility(PTV_widget.Resource.Id.indeterminateBar, Android.Views.ViewStates.Visible);
+
+		// Update UI
 		foreach (var appWidgetId in appWidgetIds)
 		{
 			appWidgetManager.UpdateAppWidget(appWidgetId, views);
@@ -38,9 +42,13 @@ public abstract class Widget : AppWidgetProvider
 		
 		try
 		{
+			// Gets location and finds closestStop of type routeType
 			info[routeType].closestStop = await GetClosestStop();
+
+			// If null, set to error state
 			if (info[routeType].closestStop == null)
 			{
+				info[routeType].stop_name = ""
 				views.SetTextViewText(PTV_widget.Resource.Id.routeName, "Not found");
 				views.SetFloat(PTV_widget.Resource.Id.routeName, "setTextSize", 16f);
 				views.SetTextViewText(PTV_widget.Resource.Id.stopName, "Not found");
@@ -51,9 +59,14 @@ public abstract class Widget : AppWidgetProvider
 				{
 					appWidgetManager.UpdateAppWidget(appWidgetId, views);
 				}
+				// Breaks out of try
 				throw new Exception("No stops found");
 			}
+			
+			// Get next unique departure for stop
 			info[routeType].currentDepartures = await APIclient.getNextDeparture(info[routeType].closestStop);
+
+			// Refresh UI
 			SetWidgetInfo(views, appWidgetManager, appWidgetIds);
 		}
 		catch { }
@@ -63,6 +76,7 @@ public abstract class Widget : AppWidgetProvider
 	{
 		try
 		{
+			// If no departures at stop, error state
 			if (info[routeType].currentDepartures.Count == 0)
 			{
 				views.SetTextViewText(PTV_widget.Resource.Id.routeName, "Not found");
@@ -72,25 +86,35 @@ public abstract class Widget : AppWidgetProvider
 				views.SetTextViewText(PTV_widget.Resource.Id.minsText, " mins");
 				throw new Exception("Not found");
 			}
+			// Check if stop name has changed, if true: reset to display first departure 
 			if (info[routeType].closestStop.stop_name[..Math.Min(11, info[routeType].closestStop.stop_name.Length)] != info[routeType].stop_name[..Math.Min(11, info[routeType].stop_name.Length)])
 				info[routeType].platformNum = 0;
 
+
 			Departure departure = info[routeType].currentDepartures[info[routeType].platformNum];
-			//Update Stop
+
+			//Update Stop if details have changed
 			if (info[routeType].closestStop.stop_name != info[routeType].stop_name || departure.destination != info[routeType].destination)
 			{
 				info[routeType].stop_name = info[routeType].closestStop.stop_name;
 				info[routeType].destination = departure.destination;
+
+				// Combines stop name and destination
 				string output = info[routeType].stop_name + " • " + info[routeType].destination;
-				if (info[routeType].stop_name.Length > 13)
+
+				// Checks length of stop name to prevent overflow
+				if (info[routeType].stop_name.Length > 13) {
 					info[routeType].stop_name = info[routeType].stop_name.Substring(0, 11) + "...";
-				output = info[routeType].stop_name + " • " + info[routeType].destination;
+					// Update output with shortened stop name
+					output = info[routeType].stop_name + " • " + info[routeType].destination;
+				}
+				//If output still overflows, shorten whole string
 				if (output.Length > 27)
 					output = output.Substring(0, 25) + "...";
 				views.SetTextViewText(PTV_widget.Resource.Id.stopName, output);
 			}
 
-			//Update route details
+			//Update route details if details have changed
 			if (departure.RouteName != info[routeType].route_name)
 			{
 				info[routeType].route_name = departure.RouteName;
@@ -98,7 +122,7 @@ public abstract class Widget : AppWidgetProvider
 				views.SetInt(PTV_widget.Resource.Id.colourStrip, "setBackgroundColor", Android.Graphics.Color.ParseColor(info[routeType].route_color));
 
 			}
-				views.SetTextViewText(PTV_widget.Resource.Id.routeName, info[routeType].route_name);
+			views.SetTextViewText(PTV_widget.Resource.Id.routeName, info[routeType].route_name);
 
 			//Update eta
 			string etaStr = " mins";
@@ -109,7 +133,10 @@ public abstract class Widget : AppWidgetProvider
 			}
 			else
 			{
+				// Get num mins until arrival
 				info[routeType].etaMins = calcEta(departure.eta);
+
+				// If eta over an hour, display hrs instead of mins
 				if (info[routeType].etaMins >= 60)
 				{
 					info[routeType].etaMins /= 60;
@@ -131,20 +158,25 @@ public abstract class Widget : AppWidgetProvider
 
 
 			}
-
+			
+			// Calculate amount of space remaining for route name
 			int availableSpace = 160 - 10 * info[routeType].etaMins.ToString().Length;
 			availableSpace += 40 - 10 * etaStr.Length;
 			if (departure.isAtPlatform)
 				availableSpace = 160;
+
+			// If name longer than 5 and less than 11 chars long, scale font size to length
 			if (info[routeType].route_name.Length > 5 && info[routeType].route_name.Length < 11)
 			{
 				views.SetFloat(PTV_widget.Resource.Id.routeName, "setTextSize", MathF.Round(availableSpace / info[routeType].route_name.Length));
 			}
+			// Shorten string if over 11 chars long
 			else if (info[routeType].route_name.Length >= 11)
 			{
 				views.SetFloat(PTV_widget.Resource.Id.routeName, "setTextSize", MathF.Round(availableSpace / 10.5f));
 				info[routeType].route_name = info[routeType].route_name.Substring(0, 10) + "...";
 			}
+			// If name <= 5 chars long, use default font size
 			else
 			{
 				views.SetFloat(PTV_widget.Resource.Id.routeName, "setTextSize", 32f);
@@ -152,13 +184,18 @@ public abstract class Widget : AppWidgetProvider
 			views.SetTextViewText(PTV_widget.Resource.Id.routeName, info[routeType].route_name);
 		}
 		catch { }
+
+		// Hide loading circle
 		views.SetViewVisibility(PTV_widget.Resource.Id.indeterminateBar, Android.Views.ViewStates.Invisible);
+
+		// Update UI
 		foreach (var appWidgetId in appWidgetIds)
 		{
 			appWidgetManager.UpdateAppWidget(appWidgetId, views);
 		}
 	}
-
+	
+	// Assign action to clickable element
 	private PendingIntent GetPendingSelfIntent(Context context, String action)
 	{
 
@@ -167,46 +204,65 @@ public abstract class Widget : AppWidgetProvider
 		return PendingIntent.GetBroadcast(context, 0, intent, PendingIntentFlags.Mutable);
 	}
 
+	// Handle button press
 	public override void OnReceive(Context? context, Intent? intent)
 	{
-        base.OnReceive(context, intent);
+        	base.OnReceive(context, intent);
+
+		// Get variables needed to call OnUpdate
 		var views = new RemoteViews(context.PackageName, Microsoft.Maui.Resource.Layout.widget);
 		AppWidgetManager appWidgetManager = AppWidgetManager.GetInstance(context);
 		ComponentName thisAppWidgetComponentName = new ComponentName(context.PackageName, this.Class.Name);
 		int[] appWidgetIds = appWidgetManager.GetAppWidgetIds(thisAppWidgetComponentName);
+
+		// If middle button (UPDATE) pressed, update widget
 		if (intent?.Action == "action.UPDATE")
 		{
 			OnUpdate(context, appWidgetManager, appWidgetIds);
 		}
+
+		// If right button (NEXT_PLATFORM) pressed, increment platformNum by 1, (overflowing to 0)
 		else if (intent?.Action == "action.NEXT_PLATFORM")
 		{
 			info[routeType].platformNum++;
+			// Overflow check
 			if (info[routeType].platformNum > info[routeType].currentDepartures.Count - 1)
 				info[routeType].platformNum = 0;
+			// Refresh UI
 			SetWidgetInfo(views, appWidgetManager, appWidgetIds);
 		}
+		
+		// If left button (PREV_PLATFORM) pressed, decrement platformNum by 1, (underflowing to final departure)
 		else if (intent?.Action == "action.PREV_PLATFORM")
 		{
 			info[routeType].platformNum--;
+			// Underflow check
 			if (info[routeType].platformNum < 0)
 				info[routeType].platformNum = info[routeType].currentDepartures.Count - 1;
+			// Refresh UI
 			SetWidgetInfo(views, appWidgetManager, appWidgetIds);
 		}
 	}
 
-    internal async Task<Stop?> GetClosestStop()
-    {
+    	internal async Task<Stop?> GetClosestStop()
+    	{
+		// Get current pos as Location obj
 		Location loc = await GetCurrentLocation();
+
+		// Returns closest stop as Stop obj
+		// If none found, returns null
 		return await APIclient.getClosestStop(loc.Longitude, loc.Latitude, routeType);
 	}
-
+	
+	// Implemented by childeren
 	internal abstract string GetRouteColour(int routeNum);
 
-
+	// Calculate num mins until arrival
 	internal int calcEta(DateTime dt)
     {
 		return (int)MathF.Ceiling((float)dt.Subtract(DateTime.Now).TotalSeconds/60f);
 	}
+
 	public async Task<Location> GetCurrentLocation()
 	{
 		try
